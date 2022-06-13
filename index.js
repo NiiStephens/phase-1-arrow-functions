2ecbde20f6e7c1bc0ce43048949533c56707cfe8
Nii <bnstephens001@gmail.com>
2022-06-13 09:19:29 +0000

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+//Write Code Here
+let divide = function(){
+    return 2000 / 100;
+}
+
+let square = (parameter1) => {
+    return parameter1 * parameter1;
+}
+
+let add = (parameter2, parameter3) => {
+    return parameter2 + parameter3;
+} 
